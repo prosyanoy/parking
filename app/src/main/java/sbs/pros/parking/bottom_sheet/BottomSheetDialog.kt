5bc89a31b0c6839af972c0d8edf361a86649c41c
thebukharian <thebukharianxo@mail.ru>
2022-08-04 19:44:36 +0500

    bottom sheet behaviour

--- app/src/main/java/sbs/pros/parking/bottom_sheet/BottomSheetDialog.kt
+++ app/src/main/java/sbs/pros/parking/bottom_sheet/BottomSheetDialog.kt
@@ -17,7 +17,7 @@ import sbs.pros.parking.R
 import sbs.pros.parking.model.PinData
 
 
-private const val COLLAPSED_HEIGHT = 228
+private const val COLLAPSED_HEIGHT = 125
 
 class BottomSheetDialog(val data: PinData ) : BottomSheetDialogFragment() {
 
@@ -75,7 +75,7 @@ class BottomSheetDialog(val data: PinData ) : BottomSheetDialogFragment() {
             behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
 
                 override fun onStateChanged(bottomSheet: View, newState: Int) {
-                   buttons.isVisible = newState == BottomSheetBehavior.STATE_EXPANDED
+                   buttons.isVisible = newState != BottomSheetBehavior.STATE_COLLAPSED
                     buttons.post {
                         (coordinator?.layoutParams as ViewGroup.MarginLayoutParams).apply {
                             buttons.measure(
