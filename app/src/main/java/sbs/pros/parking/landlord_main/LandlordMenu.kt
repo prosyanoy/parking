77bf6707aa7fb14b86e1c3ad37fd4e13333cd52b
kr1pt0 <fedorrrd@gmail.com>
2022-08-26 16:39:39 +0300

    bottom navigation

--- app/src/main/java/sbs/pros/parking/landlord_main/LandlordMenu.kt
+++ app/src/main/java/sbs/pros/parking/landlord_main/LandlordMenu.kt
@@ -1,21 +1,17 @@
 package sbs.pros.parking.landlord_main
 
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import sbs.pros.parking.landlord_main.InFragment
-import sbs.pros.parking.landlord_main.ParkedFragment
-import sbs.pros.parking.landlord_main.OutFragment
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_landlord_menu.*
 import sbs.pros.parking.R
 import sbs.pros.parking.databinding.FragmentLandlordMenuBinding
 import sbs.pros.parking.databinding.FragmentMapBinding
 import sbs.pros.parking.utils.viewLifecycleLazy
 
 class LandlordMenu : Fragment(R.layout.fragment_landlord_menu) {
-    private var bottomNavigationView: BottomNavigationView? = null
+
     private val inFragment = InFragment()
     private val parkedFragment = ParkedFragment()
     private val outFragment = OutFragment()
@@ -25,5 +21,27 @@ class LandlordMenu : Fragment(R.layout.fragment_landlord_menu) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        replaceFragment(inFragment)
+
+        bottom_menu.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.`in` -> replaceFragment(inFragment)
+                R.id.out -> replaceFragment(outFragment)
+                R.id.parked -> replaceFragment(parkedFragment)
+            }
+            true
+        }
+    }
+
+
+    private fun replaceFragment(fragment: Fragment){
+        if (fragment != null){
+
+
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
