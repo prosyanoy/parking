e139a63ec22bbc89760ec464ec4c3cb485b022b4
kr1pt0 <fedorrrd@gmail.com>
2022-08-27 00:14:27 +0300

    java -> kotlin

--- app/src/main/java/sbs/pros/parking/landlord_main/Parker.kt
+++ app/src/main/java/sbs/pros/parking/landlord_main/Parker.kt
@@ -1,27 +1,8 @@
-package sbs.pros.parking.landlord_main;
+package sbs.pros.parking.landlord_main
 
-public class Parker {
+class Parker(var carNumber: String, var startTime: String, var rating: Int) {
 
-    private String carNumber;
-    private String startTime;
-    private int rating;
-
-
-    public Parker (String carNumber, String startTime, int rating){
-        this.carNumber = carNumber;
-        this.startTime = startTime;
-        this.rating = rating;
-    }
-
-    public void setCarNumber(String carNumber) {this.carNumber = carNumber;}
-    public void setStartTime(String startTime) {this.startTime = startTime;}
-    public void setRating(int rating) {this.rating = rating;}
-    public int getRating() {return rating;}
-    public String getCarNumber() {return carNumber;}
-    public String getStartTime() {return startTime;}
-
-
-    public int getPrice(String finishTime){
+    fun getPrice(finishTime: String?): Int {
 
         /*
         get lanlord's prices
@@ -31,9 +12,6 @@ public class Parker {
         return price*time
 
          */
-
-        return 1;
+        return 1
     }
-
-
 }
