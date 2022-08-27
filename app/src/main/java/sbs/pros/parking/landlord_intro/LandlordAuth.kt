4742f9f03f8e5475d121b1f0e40e85f57fd3abd4
kr1pt0 <fedorrrd@gmail.com>
2022-08-27 11:42:44 +0300

    java -> kotlin

--- app/src/main/java/sbs/pros/parking/landlord_intro/LandlordAuth.kt
+++ app/src/main/java/sbs/pros/parking/landlord_intro/LandlordAuth.kt
@@ -1,66 +1,20 @@
-package sbs.pros.parking.landlord_intro;
+package sbs.pros.parking.landlord_intro
 
-import android.os.Bundle;
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import sbs.pros.parking.R
+import sbs.pros.parking.databinding.FragmentLandlordAuthBinding
+import sbs.pros.parking.databinding.FragmentOutBinding
+import sbs.pros.parking.utils.viewLifecycleLazy
 
-import androidx.fragment.app.Fragment;
+class LandlordAuth : Fragment(R.layout.fragment_landlord_auth){
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+    private val binding by viewLifecycleLazy { FragmentLandlordAuthBinding.bind(requireView()) }
 
-import sbs.pros.parking.R;
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LandlordAuth#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class LandlordAuth extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LandlordAuth() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LandlordAuth.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LandlordAuth newInstance(String param1, String param2) {
-        LandlordAuth fragment = new LandlordAuth();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_landlord_auth, container, false);
     }
 }
