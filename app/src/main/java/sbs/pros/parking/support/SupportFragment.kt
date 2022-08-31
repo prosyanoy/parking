256eed78621a376e257007a5363a95921ca81fae
thebukharian <thebukharianxo@mail.ru>
2022-08-31 23:57:21 +0500

    menu support

--- app/src/main/java/sbs/pros/parking/support/SupportFragment.kt
+++ app/src/main/java/sbs/pros/parking/support/SupportFragment.kt
@@ -1,6 +1,8 @@
 package sbs.pros.parking.support
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Message
 import android.view.View
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
@@ -8,6 +10,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import sbs.pros.parking.Constants
 import sbs.pros.parking.R
 import sbs.pros.parking.databinding.FragmentSupportBinding
 import sbs.pros.parking.menu.MenuViewModel
@@ -21,6 +24,7 @@ class SupportFragment() : Fragment(R.layout.fragment_support) {
     private val viewModel by activityViewModels<MenuViewModel>()
     private val binding by viewLifecycleLazy { FragmentSupportBinding.bind(requireView()) }
 
+    private var isThemeSelected = false
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,15 +39,36 @@ class SupportFragment() : Fragment(R.layout.fragment_support) {
             }
 
             messageField.addTextChangedListener {
-                it?.length?.let{ charLength -> sendBtn.isEnabled = charLength > 9 }
+                checkButton()
             }
 
 
 
             sendBtn.setSafeOnClickListener {
+                sendEmail(themeText.text.toString(), messageField.text.toString())
                 messageField.text?.clear()
             }
         }
+    }
+
+
+    private fun sendEmail(theme: String, message: String){
+        val email = Constants.EMAIL_ADDRESS
+
+
+        // define Intent object with action attribute as ACTION_SEND
+        val intent = Intent(Intent.ACTION_SEND)
+
+        // add three fields to intent using putExtra function
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        intent.putExtra(Intent.EXTRA_SUBJECT, theme)
+        intent.putExtra(Intent.EXTRA_TEXT, message)
+
+        // set type of intent
+        intent.type = "message/rfc822"
+
+        startActivity(Intent.createChooser(intent, "Choose an Email client :"))
+
     }
 
 
@@ -52,6 +77,17 @@ class SupportFragment() : Fragment(R.layout.fragment_support) {
             val theme = bundle.getString("theme")
             theme?.let { binding.themeText.text = it }
         }
+    }
+
+    private fun checkButton(){
+        val themeTxt = binding.themeText.text
+        isThemeSelected = themeTxt != "Выберите тему"
+        binding.sendBtn.isEnabled = binding.messageField.text?.length!! > 9 && isThemeSelected
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkButton()
     }
 
     companion object {
