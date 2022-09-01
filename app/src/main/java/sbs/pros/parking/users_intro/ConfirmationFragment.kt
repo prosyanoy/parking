1cb9ef653dd4e8dbb0568cf3f5f6c11a757ef05f
thebukharian <asilbuk8@gmail.com>
2022-09-01 20:17:23 +0500

    fixed confirmation view

--- app/src/main/java/sbs/pros/parking/users_intro/ConfirmationFragment.kt
+++ app/src/main/java/sbs/pros/parking/users_intro/ConfirmationFragment.kt
@@ -8,6 +8,7 @@ import android.os.CountDownTimer
 import android.view.View
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.android.volley.toolbox.StringRequest
@@ -42,19 +43,22 @@ class ConfirmationFragment : Fragment(R.layout.fragment_confirmation) {
         confirmationRequest(code)
         Toast.makeText(requireContext(), code, Toast.LENGTH_SHORT).show()
 
-        binding.codeInputView.onCompleteEventDelay = 100
 
-        binding.codeInputView.addOnCompleteListener {
-            findNavController().navigate(R.id.action_confirmationFragment_to_carRegistrationFragment)
-            if (it != code.toString()) {
+        binding.codeInputView.setCompleteListener {
+            val userInput = binding.codeInputView.text
+
+            if (userInput.length == 4 && userInput != code) {
+                binding.codeInputView.setCodeItemErrorLineDrawable()
                 Toast.makeText(requireContext(), "Invalid code", Toast.LENGTH_SHORT).show()
-                val confirmationSecond = binding.confirmationSecond
-                val c = ConstraintSet()
-                c.clone(context, R.layout.fragment_confirmation)
-                c.connect(confirmationSecond.id,ConstraintSet.TOP,binding.phoneNumber.id,ConstraintSet.BOTTOM,20)
-                c.applyTo(view.findViewById(R.id.confirmationLayout))
+//                val confirmationSecond = binding.confirmationSecond
+//                val c = ConstraintSet()
+//                c.clone(context, R.layout.fragment_confirmation)
+//                c.connect(confirmationSecond.id,ConstraintSet.TOP,binding.phoneNumber.id,ConstraintSet.BOTTOM,20)
+//                c.applyTo(view.findViewById(R.id.confirmationLayout))
             } else {
                 //haveCar(phone)
+                findNavController().navigate(R.id.action_confirmationFragment_to_carRegistrationFragment)
+
             }
         }
     }
@@ -63,8 +67,7 @@ class ConfirmationFragment : Fragment(R.layout.fragment_confirmation) {
         override fun onTick(millisUntilFinished: Long) {
             val seconds = millisUntilFinished / 1000
             try {
-                binding.repeatedRequest.text =
-                    getString(R.string.remaining1) + " $seconds " + getString(R.string.remaining2)
+                binding.repeatedRequest.text = getString(R.string.remaining1) + " $seconds " + getString(R.string.remaining2)
             }catch (e:Exception){}
 
         }
@@ -74,6 +77,8 @@ class ConfirmationFragment : Fragment(R.layout.fragment_confirmation) {
             binding.repeatedRequest.setTextColor((activity as MainActivity).blue)
 
             binding.repeatedRequest.setOnClickListener(View.OnClickListener {
+                binding.codeInputView.isVisible = true
+
                 val prefs = context!!.getSharedPreferences("time", Context.MODE_PRIVATE)
                 var attempts = prefs.getInt("attempts", 0)
                 if (attempts == 3) {
@@ -96,8 +101,8 @@ class ConfirmationFragment : Fragment(R.layout.fragment_confirmation) {
                 }
                 attempts = prefs.getInt("attempts", 0)
                 if (attempts < 3) {
-                    binding.codeInputView.code = ""
-                    binding.codeInputView.setEditable(true)
+
+                    binding.codeInputView.resetCodeItemLineDrawable()
 
                     var code = (1000..9999).random().toString()
                     confirmationRequest(code!!)
